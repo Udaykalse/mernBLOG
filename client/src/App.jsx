c131394a489942630dad38fb379bab3af9d46b60
udaykalse <udaykalse2000@gamil.com>
2024-03-07 20:44:14 +0530

    ...MERN Blog PAges

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,29 @@
+import { BrowserRouter, Routes,Route } from "react-router-dom";
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Dashboard from './pages/Dashboard/Dashboard'
+import Project from './pages/Projects/Projects'
+import Signin from  './pages/Signin/Singin'
+import Signup from  './pages/SIgnup/Signup'
+
 
 function App() {
-
   return (
     <>
-      <h1 className="text-3xl text-red-400">
-Udasay  
-      </h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/project' element={<Project/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+
+      </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+    
