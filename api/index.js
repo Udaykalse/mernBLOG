bfcf26cd1430cbc04c0e179827c782c919e6979a
udaykalse <udaykalse2000@gamil.com>
2024-03-08 23:34:32 +0530

    connection to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,23 @@
-import express from 'express';
- const app = express();
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+mongoose
+  .connect(
+   process.env.MONGO
+  )
+  .then(
+    () => {
+      console.log("Mongodb is connected ");
+    }).catch(err=>{
+        console.log(`Connection error ${err}`);
+    });
+
+const app = express();
 
 app.listen(3001, () => {
-    console.log('Server is  running on port 3001 !');
+  console.log("Server is  running on port 3001 !");
 });
