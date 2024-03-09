acee770670d68d334c5e3e4c934d100b7f06332e
udaykalse <udaykalse2000@gamil.com>
2024-03-09 23:16:11 +0530

    added middle ware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res,next) => {
   const { username, email, password } = req.body;
 
   try {
@@ -13,7 +14,8 @@ export const signup = async (req, res) => {
       email === "" ||
       password === ""
     ) {
-      return res.status(400).json({ message: "All fields are required" });
+    //   return res.status(400).json({ message: "All fields are required" });
+    next(errorHandler(400,"All fields are required"))
     }
 
     const hashPassword = bcrypt.hashSync(password, 10);
@@ -27,6 +29,7 @@ export const signup = async (req, res) => {
 
     return res.status(201).json("User signup successfully");
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    // res.status(500).json({ message: error.message });
+    next(error);
   }
 };
--- api/index.js
+++ api/index.js
@@ -31,3 +31,14 @@ app.listen(3001, () => {
 app.use('/api/user',userRoute)
 
 app.use('/api/auth',authRoute)
+
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode|| 500;
+    const message =err.message || 'Internal Server Error ';
+    res.status(statusCode).json({
+        message:message ,
+        success :false,
+        statusCode,
+        })
+
+}) 
